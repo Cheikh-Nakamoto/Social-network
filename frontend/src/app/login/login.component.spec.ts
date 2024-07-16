d481ba70c6c812567d074675e7ab81255cdd31a3
cheikhmodiouf <feppdougou@gmail.com>
2024-07-16 13:01:04 +0000

    first

--- frontend/src/app/login/login.component.spec.ts
+++ frontend/src/app/login/login.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { RightBarComponent } from './right-bar.component';
+import { LoginComponent } from './login.component';
 
-describe('RightBarComponent', () => {
-  let component: RightBarComponent;
-  let fixture: ComponentFixture<RightBarComponent>;
+describe('LoginComponent', () => {
+  let component: LoginComponent;
+  let fixture: ComponentFixture<LoginComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [RightBarComponent]
+      imports: [LoginComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(RightBarComponent);
+    fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
