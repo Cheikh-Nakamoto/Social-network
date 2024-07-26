ed08271270d0b12fe1bab83491cc97fe62e4ae21
cheikhmodiouf <feppdougou@gmail.com>
2024-07-26 10:56:46 +0000

    first

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -1,29 +1,49 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, ReactiveFormsModule } from '@angular/forms';
-
+import { DataService } from '../data.service';
+import { HttpClientModule } from '@angular/common/http';
+import { UserDTO } from '../models/models.compenant';
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule,
   ],
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss'] // Correction : 'styleUrl' en 'styleUrls'
+  styleUrls: ['./login.component.scss'],
+  providers: [DataService]
 })
 export class LoginComponent implements OnInit {
-  loginForm!: FormGroup;
-
-  constructor(private formbuilder: FormBuilder) {}
-
+  loginForm !: FormGroup;
+  user: UserDTO={
+    firstname : "madiambal",
+    lastname : "diagne",
+    email : "madiambal@gmail.com",
+    password : "123456",
+    date_of_birth : "1990-02-15",
+    nickname : "madiambal",
+    about_me : "madiambal est un développeur web passionné par les nouvelles technologies",
+    is_public : true,
+    created_at : "2022-02-15 11:20:30",
+    id: 1, // Add this line
+    avatar: "", // Add this line
+    updated_at: "" // Add this line
+  }
+  constructor(private formbuilder: FormBuilder, private apiservice: DataService) { }
   ngOnInit() {
     this.loginForm = this.formbuilder.group({
       username: [null],
       password: [null]
     });
+    // Initialisation de l'objet user
   }
-
   onlogin() {
-    // Simule une soumission de formulaire. Dans un vrai projet, vous enverrez les données au backend via un service HTTP.
-    console.log(this.loginForm.value);
+    console.log("ici c'est :",this.user)
+    this.apiservice.postData('/register', this.user).subscribe((response:any) => {
+      console.log(response);
+    }, error => {
+      console.error('Erreur lors de la connexion:', error);
+    });
   }
 }
