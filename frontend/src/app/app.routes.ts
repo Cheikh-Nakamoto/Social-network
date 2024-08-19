d0c39eb4712b3868a28cd06e75b323d8eeb5e947
cheikhmodiouf <feppdougou@gmail.com>
2024-08-19 15:58:54 +0000

    we can merge

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -6,17 +6,16 @@ import { CreatePostComponent } from './create-post/create-post.component';
 import { AuthGuard } from '../../controller/login.guard';
 import { HomeGuard } from '../../controller/home.guard';
 import { ChatComponent } from './chat/chat.component';
-import { HomeComponent } from './home/components/home/home.component';
 import { GroupeComponent } from './groupes/groupe/groupe.component';
 import { CreateGroupComponent } from './groupes/create-group/create-group.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent, canActivate: [AuthGuard] },
   { path: 'Acceuil', component: SidenavComponent, canActivate: [HomeGuard] },
-  { path: 'CreatePost', component: CreatePostComponent },
-  { path: "groups", component: GroupeComponent },
-  {path: "CreateGroup", component: CreateGroupComponent},
-  {path: "chat", component:ChatComponent },
+  { path: 'CreatePost', component: CreatePostComponent, canActivate: [HomeGuard]  },
+  { path: "groups", component: GroupeComponent , canActivate: [HomeGuard] },
+  {path: "CreateGroup", component: CreateGroupComponent, canActivate: [HomeGuard] },
+  {path: "chat", component:ChatComponent , canActivate: [HomeGuard] },
   { path: '', redirectTo: 'Acceuil', pathMatch: 'full' },
   { path: '**', redirectTo: 'Acceuil' }
 ]
