a81ef13ff732665daa0ef3914d5ced3310a8499c
cheikhmodiouf <feppdougou@gmail.com>
2024-08-29 13:58:05 +0000

    we can merge

--- backend/pkg/repository/groupRepo.go
+++ backend/pkg/repository/groupRepo.go
@@ -13,4 +13,5 @@ type GroupRepo interface {
 	CreateEventsInGroup(event dto.Events) error
 	NotificationExists(userID int) ([]dto.Notification, error)
 	CheckNotificationExists(userID, targetID int, message string) (bool, error)
+	GetNotificationsByUserID(userID int) ([]dto.Notification, error)
 }
