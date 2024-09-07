cf53ea9c9f3e53deb6dba1a6b8df90f493037cde
cheikhmodiouf <feppdougou@gmail.com>
2024-09-07 00:45:28 +0000

    we can merge

--- frontend/src/app/groupes/groupe/groupe.component.ts
+++ frontend/src/app/groupes/groupe/groupe.component.ts
@@ -29,17 +29,7 @@ export class GroupeComponent implements OnInit, OnDestroy {
   groupeForm!: FormGroup;
   id !: string;
   clear!: any;
-  selectedFile: File | null = null;
-  AllUser: AllUsersDTO = {};
-  posts: Post[] = [];
-  share: number = 0;
-  comments: CommentContent = { comments_by_post: {} };
-  likemap = [];
-  dislikemap = [];
-  token = localStorage.getItem('token');
-  user: any;
-  postAndButton!: Posts;
-  comlength : length = {}
+
 
   constructor(private fb: FormBuilder, private groupService: DataService, private router: Router, private authService: AuthService) { }
 
@@ -87,19 +77,7 @@ export class GroupeComponent implements OnInit, OnDestroy {
     );
   }
 
-  getAllPosts(): void {
-    this.groupService.getData('AllPost').subscribe(
-      (response: Post[]) => {
-        this.posts = response;
-        console.log('ici sont les posts', this.posts);
-        this.loadLikes('post');
-        this.loadDislikes('post');
-      },
-      (error) => {
-        console.error('Error fetching posts:', error);
-      }
-    );
-  }
+ 
 
 
 
@@ -124,108 +102,7 @@ export class GroupeComponent implements OnInit, OnDestroy {
     }, (error) => console.error('Error fetching ', error))
   }
 
-  // onLike(targetId: number, targetType: string) {
-  //   this.groupService.likeTarget(0, this.id, targetId, targetType, true).subscribe((response) => {
-  //     console.log("like response ", response);
-  //     this.loadLikes(targetType);
-  //     this.loadDislikes(targetType);
-  //   });
-  // }
-
-  // onDislike(targetId: number, targetType: string) {
-  //   this.groupService.dislikeTarget(0, this.id, targetId, targetType, false).subscribe(() => {
-  //     this.loadLikes(targetType);
-  //     this.loadDislikes(targetType);
-  //   });
-  // }
-  onFileSelected(event: any) {
-    const file: File = event.target.files[0];
-    if (file) {
-      this.selectedFile = file;
-    }
-  }
-
-  onComment(postId: number, targetType: string, event: Event) {
-    event.preventDefault();
-
-    const target = event.target as HTMLFormElement;
-    const content = (target.querySelector('input[name="comment"]') as HTMLInputElement).value;
-
-    if (!content) {
-      return;
-    }
-
-    const formData = new FormData();
-    formData.append('user_id', this.id.toString());
-    formData.append('target_id', postId.toString());
-    formData.append('content', content);
-    formData.append('target_type', targetType);
-
-    if (this.selectedFile) {
-      formData.append('image', this.selectedFile); // Ajoutez l'image au formulaire si elle existe
-    }
-
-    this.groupService.postData('CreateComment', formData).subscribe(() => {
-      (target.querySelector('input[name="comment"]') as HTMLInputElement).value = '';
-      this.loadComments();
-      this.selectedFile = null; // Réinitialiser après l'envoi
-    });
-  }
-
-  private loadComments(): void {
-    this.groupService.getData('AllComments').subscribe(
-      (comment: {
-        Comments: { [key: number]: CommentDTO[] }
-        CommentsLength: { [key: number]: number }
-      }) => {
-        this.comments.comments_by_post = comment.Comments;
-        this.comlength = comment.CommentsLength
-        console.log('ici sont les commentaires', comment);
-      },
-      error => {
-        console.error('Erreur lors du chargement des commentaires:', error);
-      }
-    );
-  }
-
-  private loadLikes(targetType: string) {
-    this.groupService.getTargetLikes(targetType).subscribe((likes) => {
-      this.likemap = likes;
-    });
-  }
-
-  private loadDislikes(targetType: string) {
-    this.groupService.getTargetDislikes(targetType).subscribe((dislikes) => {
-      this.dislikemap = dislikes;
-    });
-  }
-
-  private loadUser(targetlink: string) {
-    this.groupService.getData(targetlink).subscribe((user: AllUsersDTO) => {
-      this.AllUser = user;
-      console.log('ici sont les utilisateurs', this.AllUser);
-    });
-  }
-
-  readonly dialog = inject(MatDialog);
-
-  openDialog(postId: number): void {
-    // Récupérer les commentaires pour le post spécifié
-    const comment = this.comments.comments_by_post[postId] || [];
-
-    // Ouvrir le dialogue avec les commentaires pour le post
-    const dialogRef = this.dialog.open(DialogCommentComponent, {
-      data: {
-        postId: postId,
-        user: this.AllUser,
-        comments: comment
-      }
-    });
-
-    dialogRef.afterClosed().subscribe(result => {
-      console.log(`Dialog result: ${result}`);
-    });
-  }
+ 
 
   handleClick(route: string, event: Event, id?: number): void {
     event.preventDefault();
