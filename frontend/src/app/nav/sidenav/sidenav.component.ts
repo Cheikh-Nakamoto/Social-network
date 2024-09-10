424b283f5bc32c109a287b1fc3b702a9d50540ca
Franchis Janel MOKOMBA <janel affranchis@gmail.com>
2024-09-10 13:42:19 +0000

    Janel has pushed!

--- frontend/src/app/nav/sidenav/sidenav.component.ts
+++ frontend/src/app/nav/sidenav/sidenav.component.ts
@@ -43,6 +43,7 @@ export class SidenavComponent implements OnInit {
     menuItems = [
         { name: 'Home', route: '/', icon: 'icofont-ui-home' },
         { name: 'Profile', route: '/profile/1', icon: 'icofont-user' },
+        { name: 'Suggestions', route: '/suggestions', icon: 'icofont-users-alt-3' },
         { name: 'Friends', route: '/followers', icon: 'icofont-users-alt-4' },
         { name: 'Groups', route: '/groups', icon: 'icofont-users-social' },
     ];
