bf10464eddc5ace050e51987fb60c1b0cb8c0ba9
cheikhmodiouf <feppdougou@gmail.com>
2024-09-10 12:51:46 +0000

    we can merge

--- push.sh
+++ push.sh
@@ -6,6 +6,7 @@ while true; do
     git stash apply
     git add -u
     git commit -m "we can merge"
+    git pull
     git push
     # Attendre 30 minutes (1800 secondes) avant de répéter
     sleep 1200
